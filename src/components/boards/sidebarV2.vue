50464bffd65e8bda1b069de4cb894f9272ba6057
Chaiy0 <p50224.2014@gmail.com>
2024-12-08 21:49:17 +0700

    แก้ svg

--- src/components/boards/sidebarV2.vue
+++ src/components/boards/sidebarV2.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted, computed, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import { ArrowRightCircleIcon,ArrowLeftCircleIcon } from '@heroicons/vue/24/solid'
+import { ArrowRightCircleIcon,ArrowLeftCircleIcon, ClipboardDocumentIcon, EyeIcon, ChevronDownIcon, UserIcon, UserGroupIcon, SquaresPlusIcon, ArrowLeftStartOnRectangleIcon } from '@heroicons/vue/24/solid'
 import {
   checkUserInAuthToken,
   checkAuthToken,
@@ -83,17 +83,7 @@ async function logOut() {
         <div
           class="flex flex-col items-start justify-start first-letter:mx-auto space-x-4 pt-5"
         >
-        
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="#FFA500"
-            class="size-6"
-          >
-            <path
-              d="M6 3a3 3 0 0 0-3 3v2.25a3 3 0 0 0 3 3h2.25a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3H6ZM15.75 3a3 3 0 0 0-3 3v2.25a3 3 0 0 0 3 3H18a3 3 0 0 0 3-3V6a3 3 0 0 0-3-3h-2.25ZM6 12.75a3 3 0 0 0-3 3V18a3 3 0 0 0 3 3h2.25a3 3 0 0 0 3-3v-2.25a3 3 0 0 0-3-3H6ZM17.625 13.5a.75.75 0 0 0-1.5 0v2.625H13.5a.75.75 0 0 0 0 1.5h2.625v2.625a.75.75 0 0 0 1.5 0v-2.625h2.625a.75.75 0 0 0 0-1.5h-2.625V13.5Z"
-            />
-          </svg>
+        <SquaresPlusIcon class="size-7 text-orange-400"/>
         </div>
         <div class="p-5">
           <h3
@@ -108,24 +98,11 @@ async function logOut() {
         <!-- PersonalBoard Section -->
         <div
           class="w-60 p-5 flex items-center justify-between cursor-pointer"
-          :class="['rounded-md', { 'bg-[#f7f285]': isBoardPage }]"
+          :class="['rounded-md', { 'bg-orange-400': isBoardPage }]"
           @click="togglePersonalDropdown()"
         >
           <div class="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              :stroke="isBoardPage ? 'white' : 'gray'"
-              class="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122"
-              />
-            </svg>
+            <ClipboardDocumentIcon class="size-7"/>
             <p
               class="pl-3"
               :class="{
@@ -136,20 +113,7 @@ async function logOut() {
               Personal Board
             </p>
           </div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            :stroke="isBoardPage ? 'white' : 'gray'"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              :d="isPersonalDropdownOpen ? 'M6 15l6-6 6 6' : 'M6 9l6 6 6-6'"
-            />
-          </svg>
+          <ChevronDownIcon class="size-5"/>
         </div>
 
         <!-- Dropdown for PersonalBoard -->
@@ -182,20 +146,7 @@ async function logOut() {
           @click="toggleCollabDropdown()"
         >
           <div class="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              :stroke="isBoardPage ? 'white' : 'gray'"
-              class="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122"
-              />
-            </svg>
+            <UserGroupIcon class="size-7"/>
             <p
               class="pl-3"
               :class="{
@@ -206,20 +157,7 @@ async function logOut() {
               Collaborator Board
             </p>
           </div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            :stroke="isBoardPage ? 'white' : 'gray'"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              :d="isPersonalDropdownOpen ? 'M6 15l6-6 6 6' : 'M6 9l6 6 6-6'"
-            />
-          </svg>
+          <ChevronDownIcon class="size-5"/>
         </div>
 
         <!-- Dropdown for CollabBoards -->
@@ -252,20 +190,7 @@ async function logOut() {
           @click="togglePublicDropdown()"
         >
           <div class="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              :stroke="isBoardPage ? 'white' : 'gray'"
-              class="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122"
-              />
-            </svg>
+            <EyeIcon class="size-7"/>
             <p
               class="pl-3"
               :class="{
@@ -276,20 +201,7 @@ async function logOut() {
               Public Board
             </p>
           </div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            :stroke="isBoardPage ? 'white' : 'gray'"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              :d="isPersonalDropdownOpen ? 'M6 15l6-6 6 6' : 'M6 9l6 6 6-6'"
-            />
-          </svg>
+          <ChevronDownIcon class="size-5"/>
         </div>
 
         <!-- Dropdown for PublicBoards -->
@@ -326,20 +238,7 @@ async function logOut() {
         @click="logOut()"
       >
         <div class="flex items-center space-x-2 p-1" v-show="checkAuthToken()">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-          </svg>
+          <UserIcon class="size-5"/>
         </div>
         <p class="itbkk-fullname text-sm font-medium p-2">
           {{ checkAuthToken() ? username : "Login" }}
@@ -348,20 +247,7 @@ async function logOut() {
           class="flex items-center justify-center right-0"
           v-show="checkAuthToken()"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-5"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
-            />
-          </svg>
+        <ArrowLeftStartOnRectangleIcon class="size-5"/>
         </div>
       </div>
     </div>
